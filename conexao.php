ad6416ae19f7d07623a2000b4df9e4917f6ed13e
caiquegl <arq.caique@hotmail.com>
2020-11-24 20:51:41 -0300

    deploy

--- conexao.php
+++ conexao.php
@@ -1,7 +1,8 @@
 <?php
-define('HOST', 'localhost');
-define('USUARIO', 'root');
-define('SENHA', '');
-define('DB', 'aulaPhp');
+define('HOST', 'us-cdbr-east-02.cleardb.com');
+define('USUARIO', 'b14fcc4801580b');
+define('SENHA', '53ab1dc3');
+define('PORT', '3306');
+define('DB', 'heroku_0cf7ed56596fa45');
 
-$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
+$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB, PORT) or die ('Não foi possível conectar');
