80727794202c35f57d9fdbdefa61cc74cc90a7ce
caiquegl <arq.caique@hotmail.com>
2020-11-24 21:32:31 -0300

    debug

--- login.php
+++ login.php
@@ -10,7 +10,7 @@ if(empty($_POST['usuario']) || empty($_POST['senha'])) {
 $usuario = mysqli_real_escape_string($conexao, $_POST['usuario']);
 $senha = mysqli_real_escape_string($conexao, $_POST['senha']);
 
-$query = "select nome from usuario where usuario = '{$usuario}' and senha = md5('{$senha}')";
+$query = "select nome from usuario where usuario = '{$usuario}' and senha = '{$senha}'";
 
 $result = mysqli_query($conexao, $query);
 
@@ -19,10 +19,10 @@ $row = mysqli_num_rows($result);
 if($row == 1) {
 	$usuario_bd = mysqli_fetch_assoc($result);
 	$_SESSION['nome'] = $usuario_bd['nome'];
-	header('Location: painel.php');
+	include('home.html');
 	exit();
 } else {
 	$_SESSION['nao_autenticado'] = true;
-	header('Location: index.php');
+	header('Location: home.php');
 	exit();
 }
