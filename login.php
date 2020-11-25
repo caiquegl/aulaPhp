758e5b33e1e7942d4df8f1e5c43615e089fb5183
caiquegl <arq.caique@hotmail.com>
2020-11-24 21:07:08 -0300

    debug

--- login.php
+++ login.php
@@ -1,37 +1,22 @@
 <?php
-  $email = $_POST["email"];
-  $senha = $_POST["senha"];
+$email =  mysqli_real_escape_string($conexao, trim($_POST['email']));
+$senha =  mysqli_real_escape_string($conexao, trim($_POST['senha']));
+  include("conexao.php");
 
-  $servername = "localhost";
-  $username = "root";
-  $password = "";
+  $sql = "select count(*) as total from usuario where email = '$email' AND senha = '$senha'";
   
-  try {
-    $conn = new PDO("mysql:host=$servername;dbname=aulaPhp", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "Conexão realizada com sucesso.";
+  $result = mysqli_query($conexao, $sql);
+$row = mysqli_fetch_assoc($result);
 
-    $stmt = $conn->prepare("SELECT id FROM usuario WHERE email=:email AND senha=:senha");
-    $stmt->bindParam(':email', $email, PDO::PARAM_STR);
-    $stmt->bindParam(':senha', $senha, PDO::PARAM_STR);
-    $stmt->execute();
-    // set the resulting array to associative
-    $result = $stmt->fetchAll();
-    $qtd_usuario = count($result);
-
-    if($qtd_usuario == 1){
-      include("home.html");
-
-    }else{
-      $resultado["msg"] = "Usuário não encontrado";
-      $resultado["cod"] = 0;
-      include("index.php");
-    }
-
-  } catch(PDOException $e) {
-    echo "Falha na conexão: " . $e->getMessage();
-  }
+if($row['total'] == 1){
+  include("home.html");
+}else{
+  $resultado["msg"] = "Usuário não encontrado";
+  $resultado["cod"] = 0;
+  include("index.php");
+}
+  
+  
   $conn = null;
 
 ?>
